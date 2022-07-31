1798a64a5183328382d90f83de4a760ba822b280
ShakyaB04 <shakyabhattacharyya@gmail.com>
2022-07-31 12:11:29 +0530

    done by Shakya Bhattacharyya

--- index.js
+++ index.js
@@ -1,26 +1,54 @@
 class SortedList {
   constructor() {
-  
+    this.items = [],
+    this.length = []
   }
-  add() {
-   
+  add(item) {
+    var newitem = this.items;
+    newitem.push(item);
+    newitem.sort((a,b) => a>b ? 1 : -1)
+    this.items = newitem;
+    this.length = this.items.length;
   }
-  get() {
-
+  get(pos) {
+    if(pos>this.length-1){
+      throw new Error('OutofBounds');
+    }
+    else{
+      return this.items[pos];
+    }
   }
   max() {
-
+    if(this.length==0){
+      throw new Error('EmptySortedList');
+    }
+    else{
+      return Math.max(...this.items);
+    }
   }
   min() {
-
-  }
-  avg() {
-    
-   
+    if(this.length==0){
+      throw new Error('EmptySortedList');
+    }
+    else{
+      return Math.min(...this.items);
+    }
   }
 
   sum() {
-  
+    var sum = this.items.reduce((total,val) => {
+      return total + val;
+    }, 0)
+    return sum;
+  }
+
+  avg() {
+    if(this.length==0){
+      throw new Error('EmptySortedList')
+    }
+    else{
+      return this.sum()/this.length;
+    }
   }
   
 };
